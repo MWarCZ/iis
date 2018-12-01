8b29e86c0408b7ef91dc51422658549764edd967
TheElesty <32326985+TheElesty@users.noreply.github.com>
2018-12-01 20:08:27 +0100

    DB update

--- xvalka05_xzajic16.sql
+++ xvalka05_xzajic16.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 01, 2018 at 09:54 AM
+-- Generation Time: Dec 01, 2018 at 08:06 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.12
 
@@ -32,6 +32,17 @@ CREATE TABLE `accesses` (
   `idAccess` int(11) NOT NULL,
   `description` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `accesses`
+--
+
+INSERT INTO `accesses` (`idAccess`, `description`) VALUES
+(1, 'G'),
+(2, 'PG'),
+(3, 'PG-13'),
+(4, 'R'),
+(5, 'NC-17');
 
 -- --------------------------------------------------------
 
@@ -65,6 +76,13 @@ CREATE TABLE `cinemas` (
   `name` varchar(100) NOT NULL,
   `address` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `cinemas`
+--
+
+INSERT INTO `cinemas` (`idCinema`, `name`, `address`) VALUES
+(1, 'asdasd', 'asdasda');
 
 -- --------------------------------------------------------
 
@@ -108,7 +126,8 @@ INSERT INTO `discounts` (`idDiscount`, `description`, `discount`) VALUES
 (2, 'Dítě (do 12let)', 50),
 (3, 'Student (do 26let)', 25),
 (4, 'Důchodce (nad 65)', 25),
-(5, 'ZTP', 25);
+(5, 'ZTP', 25),
+(7, 'aaasdasdaa', 99);
 
 -- --------------------------------------------------------
 
@@ -156,8 +175,7 @@ CREATE TABLE `films` (
 
 INSERT INTO `films` (`idFilm`, `name`, `duration`, `released`, `ratings`, `idDirector`, `idStudio`) VALUES
 (1, 'awesdrtfhgds', 150, '2018-11-01', 15, 1, 1),
-(2, 'asdasda', 150, '2018-11-01', 15, 2, 1),
-(3, 'free', 150, '2018-11-01', 15, NULL, NULL);
+(2, 'asdasda', 150, '2018-11-01', 15, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -233,6 +251,13 @@ CREATE TABLE `halls` (
   `idCinema` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `halls`
+--
+
+INSERT INTO `halls` (`idHall`, `cinemaMark`, `capacity`, `idCinema`) VALUES
+(1, 'asd', 10, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -252,7 +277,9 @@ CREATE TABLE `prices` (
 INSERT INTO `prices` (`idPrice`, `description`, `price`) VALUES
 (1, '2D', 99),
 (2, '3D', 149),
-(3, 'Předpremiéra', 199);
+(3, 'Předpremiéra', 199),
+(5, 'aaaa', 2000),
+(6, 'aaaa', 2000);
 
 -- --------------------------------------------------------
 
@@ -262,23 +289,23 @@ INSERT INTO `prices` (`idPrice`, `description`, `price`) VALUES
 
 CREATE TABLE `projections` (
   `idProjection` int(11) NOT NULL,
+  `date` datetime NOT NULL,
   `idFilm` int(11) NOT NULL,
   `idAccess` int(11) NOT NULL,
-  `idPrice` int(11) NOT NULL
+  `idPrice` int(11) NOT NULL,
+  `idHall` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `proj_hall`
+-- Dumping data for table `projections`
 --
 
-CREATE TABLE `proj_hall` (
-  `idPH` int(11) NOT NULL,
-  `idProjection` int(11) NOT NULL,
-  `idHall` int(11) NOT NULL,
-  `datetime` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `projections` (`idProjection`, `date`, `idFilm`, `idAccess`, `idPrice`, `idHall`) VALUES
+(5, '2018-01-01 18:40:00', 1, 1, 1, 1),
+(8, '2018-12-01 00:00:00', 2, 1, 1, 1),
+(9, '2018-12-01 00:00:00', 2, 1, 1, 1),
+(10, '2018-12-01 00:00:00', 1, 1, 1, 1),
+(11, '2018-12-01 00:00:00', 1, 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -293,6 +320,16 @@ CREATE TABLE `reservations` (
   `picked` tinyint(1) NOT NULL,
   `idUser` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `reservations`
+--
+
+INSERT INTO `reservations` (`idReservation`, `reserved`, `paid`, `picked`, `idUser`) VALUES
+(3, '2018-12-01 18:35:35', 0, 0, 1),
+(4, '2018-12-01 18:35:35', 0, 0, 1),
+(5, '2018-12-01 18:35:35', 0, 0, 2),
+(6, '2018-12-01 18:35:35', 0, 0, 2);
 
 -- --------------------------------------------------------
 
@@ -328,6 +365,23 @@ CREATE TABLE `tickets` (
   `seatNumber` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `tickets`
+--
+
+INSERT INTO `tickets` (`idTicket`, `price`, `idReservation`, `idDiscount`, `idProjection`, `seatNumber`) VALUES
+(1, 199, 3, 1, 5, 1),
+(2, 199, 3, 1, 5, 1),
+(3, 199, 3, 1, 8, 1),
+(4, 199, 4, 1, 8, 1),
+(5, 199, 5, 1, 5, 1),
+(6, 199, 5, 1, 5, 1),
+(7, 199, 5, 1, 9, 1),
+(8, 199, 6, 1, 5, 1),
+(9, 199, 6, 1, 9, 1),
+(10, 199, 6, 1, 10, 1),
+(11, 199, 6, 1, 10, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -339,7 +393,7 @@ CREATE TABLE `users` (
   `login` varchar(100) NOT NULL,
   `name` varchar(16) NOT NULL,
   `surname` varchar(16) NOT NULL,
-  `email` varchar(100) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
   `passwd` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -348,7 +402,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`idUser`, `login`, `name`, `surname`, `email`, `passwd`) VALUES
-(1, 'test', 'test', 'test', 'tes@test.test', '$2y$10$zwwWQjobBnuXBriBRkcinOq6CgRYQ05DTc7/.SCDSd8azy3do.6CW');
+(1, 'test', 'test', 'test', 'tes@test.test', '$2y$10$zwwWQjobBnuXBriBRkcinOq6CgRYQ05DTc7/.SCDSd8azy3do.6CW'),
+(2, 'asdasd', 'asdasd', 'asdasd', 'asdasda', 'zasdasd');
 
 --
 -- Indexes for dumped tables
@@ -441,14 +496,7 @@ ALTER TABLE `projections`
   ADD PRIMARY KEY (`idProjection`),
   ADD KEY `idAccess` (`idAccess`),
   ADD KEY `idFilm` (`idFilm`),
-  ADD KEY `idPrice` (`idPrice`);
-
---
--- Indexes for table `proj_hall`
---
-ALTER TABLE `proj_hall`
-  ADD PRIMARY KEY (`idPH`),
-  ADD KEY `idProjection` (`idProjection`),
+  ADD KEY `idPrice` (`idPrice`),
   ADD KEY `idHall` (`idHall`);
 
 --
@@ -488,7 +536,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `accesses`
 --
 ALTER TABLE `accesses`
-  MODIFY `idAccess` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idAccess` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `actors`
@@ -500,7 +548,7 @@ ALTER TABLE `actors`
 -- AUTO_INCREMENT for table `cinemas`
 --
 ALTER TABLE `cinemas`
-  MODIFY `idCinema` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idCinema` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `directors`
@@ -512,7 +560,7 @@ ALTER TABLE `directors`
 -- AUTO_INCREMENT for table `discounts`
 --
 ALTER TABLE `discounts`
-  MODIFY `idDiscount` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `idDiscount` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `employees`
@@ -548,31 +596,25 @@ ALTER TABLE `genres`
 -- AUTO_INCREMENT for table `halls`
 --
 ALTER TABLE `halls`
-  MODIFY `idHall` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idHall` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `prices`
 --
 ALTER TABLE `prices`
-  MODIFY `idPrice` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idPrice` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `projections`
 --
 ALTER TABLE `projections`
-  MODIFY `idProjection` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `proj_hall`
---
-ALTER TABLE `proj_hall`
-  MODIFY `idPH` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idProjection` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `reservations`
 --
 ALTER TABLE `reservations`
-  MODIFY `idReservation` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idReservation` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `studios`
@@ -584,13 +626,13 @@ ALTER TABLE `studios`
 -- AUTO_INCREMENT for table `tickets`
 --
 ALTER TABLE `tickets`
-  MODIFY `idTicket` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idTicket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -635,14 +677,8 @@ ALTER TABLE `halls`
 ALTER TABLE `projections`
   ADD CONSTRAINT `projections_ibfk_1` FOREIGN KEY (`idAccess`) REFERENCES `accesses` (`idAccess`),
   ADD CONSTRAINT `projections_ibfk_2` FOREIGN KEY (`idFilm`) REFERENCES `films` (`idFilm`),
-  ADD CONSTRAINT `projections_ibfk_3` FOREIGN KEY (`idPrice`) REFERENCES `prices` (`idPrice`);
-
---
--- Constraints for table `proj_hall`
---
-ALTER TABLE `proj_hall`
-  ADD CONSTRAINT `proj_hall_ibfk_2` FOREIGN KEY (`idHall`) REFERENCES `halls` (`idHall`),
-  ADD CONSTRAINT `proj_hall_ibfk_3` FOREIGN KEY (`idProjection`) REFERENCES `projections` (`idProjection`);
+  ADD CONSTRAINT `projections_ibfk_3` FOREIGN KEY (`idPrice`) REFERENCES `prices` (`idPrice`),
+  ADD CONSTRAINT `projections_ibfk_4` FOREIGN KEY (`idHall`) REFERENCES `halls` (`idHall`);
 
 --
 -- Constraints for table `reservations`
