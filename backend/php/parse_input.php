79453834d18670cca168c65ca060503f14377373
TheElesty <32326985+TheElesty@users.noreply.github.com>
2018-12-01 13:04:18 +0100

    Update parse_input.php

--- backend/php/parse_input.php
+++ backend/php/parse_input.php
@@ -2,19 +2,19 @@
 require_once './php/debug.php';
 
 //Start session
-session_set_cookie_params(33600, '/', '.iis.mwarcz.cz', FALSE, FALSE);
+session_set_cookie_params(33600, '/', '.mwarcz.cz', FALSE, TRUE);
 session_start();
 
 //BYPAS TODO remove
-$_SESSION = array(
-            "id"=>      1,
-            "login"=>   "admin",
-            "name"=>    "admin",
-            "surname"=> "admin",
-            "ssn"=>     "999999/9999",
-            "email"=>   "emile@emile.com",
-            "access"=>  4
-        );
+//$_SESSION = array(
+//            "id"=>      1,
+//            "login"=>   "admin",
+//            "name"=>    "admin",
+//            "surname"=> "admin",
+//            "ssn"=>     "999999/9999",
+//            "email"=>   "emile@emile.com",
+//            "access"=>  4
+//        );
 
 //Parse POST data
 if(isset($_REQUEST["request"])) {
