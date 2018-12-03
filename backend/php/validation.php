ade3591873d4d64ec71c768faf76c5c2a08ca6ac
TheElesty <32326985+TheElesty@users.noreply.github.com>
2018-12-03 09:42:44 +0100

    SSN validation

--- backend/php/validation.php
+++ backend/php/validation.php
@@ -4,17 +4,19 @@ function validate_email($email) {
 }
 
 function validate_ssn($ssn) {
-    if(preg_match("/^([0-9]{6})(\/)?([0-9]{4})$/", $ssn)) {
-        if(strlen($ssn) == 11) $ssn = str_replace('/', '', $ssn);
-        
-        $a = $b = 0;
-        for($i = 0; $i < 10 ;$i += 2) {
-            $a += intval(substr($ssn, $i, 1));
-            $b += intval(substr($ssn, $i+1, 1));
-        }
-        return (($a - $b) % 11 == 0) ? true : false;
-        
-    } else if(preg_match("/^([0-9]{6})(\/)?([0-9]{3})$/", $ssn)) return true;
+//    if(preg_match("/^([0-9]{6})(\/)?([0-9]{4})$/", $ssn)) {
+//        if(strlen($ssn) == 11) $ssn = str_replace('/', '', $ssn);
+//        
+//        $a = $b = 0;
+//        for($i = 0; $i < 10 ;$i += 2) {
+//            $a += intval(substr($ssn, $i, 1));
+//            $b += intval(substr($ssn, $i+1, 1));
+//        }
+//        return (($a - $b) % 11 == 0) ? true : false;
+//        
+//    } else 
+    
+    if(preg_match("/^([0-9]{6})(\/)?([0-9]{3-4})$/", $ssn)) return true;
     
     return false;
 }
